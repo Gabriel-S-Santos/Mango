f45b86943d873b594869f0dcd87e5098880e70c4
Gabriel Santos <gabrielsilvadossantos3@gmail.com>
2024-12-11 09:54:38 -0300

    Adicionar arquivos de projeto.

---
+++ Mango.Services.CumponAPI/Controllers/CuponAPIControler.cs
@@ -0,0 +1,35 @@
+﻿using Mango.Services.CuponAPI.Data;
+using Mango.Services.CuponAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.CuponAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CuponAPIControler : ControllerBase
+    {
+        private readonly AppDbContext _db;
+
+        public CuponAPIControler(AppDbContext db) 
+        {
+            _db = db;
+        }
+
+
+        [HttpGet]
+        public object Get() 
+        {
+            try
+            {
+               IEnumerable<Cupon> objList = _db.Cupons.ToList();
+                return objList;
+            }
+            catch (Exception ex) 
+            {
+                
+            }
+            return null;
+        }
+    }
+}
---
+++ Mango.Services.CumponAPI/Models/Cupon.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Services.CuponAPI.Models
+{
+    public class Cupon
+    {
+        [Key]
+        public int CuponId { get; set; }
+        [Required]
+        public string CuponCode { get; set; }
+        [Required]
+        public double DiscountAmount {  get; set; }
+        public int MinAmount { get; set; }
+    }
+}
